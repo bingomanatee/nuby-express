4292d66e98b1dc5758933fd4ec788e86f88bed36
Dave Edelhart <dave@wonderlandlabs.com>
2012-05-06 06:12:06 -0700

    renaming resource types; exposing handlers, path handler at higher levels.

---
+++ components/com_layout/resources/mixins/rescan_layouts.js
@@ -0,0 +1,33 @@
+var Gate = require('support/gate');
+var NE = require('./../../../../lib');
+
+module.exports = {
+    init:function (frame, cb) {
+
+        var layout_handler = NE.handlers.Resource_Type('layout');
+
+        var layout_dir_handler = new NE.Path_Handler({
+            name: 'layout_dir_handler',
+            re: /layout(s)?/,
+            type: 'dir',
+            execute: function(props, cb){
+                var layout_loader = new NE.Loader({path: props.full_path});
+                layout_loader.add_handler(layout_handler);
+                layout_loader.start_load(function(){
+                    frame.add_resources(layout_loader.get_resources('layout'));
+                    cb();
+                });
+            }
+        })
+
+        var gate = new Gate(cb);
+
+            var loader = new NE.Loader({path: frame.path});
+            loader.add_handler(layout_dir_handler);
+            gate.task_start();
+            loader.load(function(){
+                frame.add_resources(loader.get_resources('layout'));
+                gate.task_done();
+            })
+    }
+}
